fe3e60572a0c4f79a48b149fbcde177e57a15041
Erik Engervall <erik.engervall@gmail.com>
2023-10-23 21:20:21 +0900

    Remove deprecated jest option

--- jest.config.ts
+++ jest.config.ts
@@ -5,7 +5,6 @@ const config: Config = {
   testEnvironment: 'node',
   testPathIgnorePatterns: ['/node_modules/'],
   roots: ['<rootDir>/src-node'],
-  projects: ['<rootDir>/src-node'],
   prettierPath: require.resolve('prettier-2'),
 }
 
